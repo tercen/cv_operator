fed35e4e5b79fbb72c03675de822a59ff8a5c0cb
Martin English <64135638+martin1english@users.noreply.github.com>
2021-06-18 12:09:17 +0100

    Update renv and worksflow

--- workspace.R
+++ workspace.R
@@ -1,6 +1,9 @@
 library(tercen)
 library(dplyr)
 
+options("tercen.workflowId" = "6015a4dd34cef273755e1a1b1500427b")
+options("tercen.stepId"     = "d31241f6-173f-473a-9307-2b4b3c5c0882")
+
 ctx <- tercenCtx()
 
 ctx  %>% 
